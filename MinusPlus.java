5ee0d950c24c2b747a61861bffbd46b30147a49c
HarideVos <53867807+HarideVos@users.noreply.github.com>
2019-08-07 15:56:02 -0400

    Add files via upload

---
+++ MinusPlus.java
@@ -0,0 +1,12 @@
+
+public class MinusPlus {
+	public static void main(String[] args) {
+		int a, b, c, tempA, result;
+		a = 20;
+		b = 10;
+		c = -1;
+		tempA = a * c;
+		result = (tempA - b) * c;
+		System.out.println(result);
+	}
+}
